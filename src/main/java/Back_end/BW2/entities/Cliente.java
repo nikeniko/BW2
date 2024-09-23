9e4df4ea0e7920d9f8f74ed159b70d78b8864c96
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-09-23 12:59:46 +0200

    aggiunte entities
    creata utente e cliente

---
+++ src/main/java/Back_end/BW2/entities/Cliente.java
@@ -0,0 +1,77 @@
+package Back_end.BW2.entities;
+
+import Back_end.BW2.enums.TipoAzienda;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "clienti")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+    @Column(name = "ragione_sociale")
+    private String ragioneSociale;
+    @Column(name = "partita_iva")
+    private long partitaIva;
+    @Column(name = "data_inserimento")
+    private LocalDate dataInserimento;
+    @Column(name = "data_ultimo_contatto")
+    private LocalDate dataUltimoContatto;
+    @Column(name = "fatturato_annuale")
+    private long fatturatoAnnuale;
+    private String pec;
+    private long telefono;
+    @Column(name = "email_contatto")
+    private String emailContatto;
+    @Column(name = "nome_contatto")
+    private String nomeContatto;
+    @Column(name = "cognome_contatto")
+    private String cognomeContatto;
+    @Column(name = "logo_aziendale")
+    private String logoAziendale;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_azienda")
+    private TipoAzienda tipoAzienda;
+
+
+    @OneToMany
+    @JoinColumn(name = "fattureList")
+    private List<Fattura> fattureList;
+
+    @OneToOne
+    private Indirizzo indirizzoSedeLegale;
+
+    @OneToOne
+    private Indirizzo indirizzoSedeOperativa;
+
+    public Cliente(String ragioneSociale, long partitaIva, LocalDate dataInserimento, LocalDate dataUltimoContatto,
+                   long fatturatoAnnuale, String pec, long telefono, String emailContatto, String nomeContatto, String cognomeContatto,
+                   String logoAziendale, TipoAzienda tipoAzienda, Indirizzo indirizzoSedeLegale, Indirizzo indirizzoSedeOperativa) {
+        this.ragioneSociale = ragioneSociale;
+        this.partitaIva = partitaIva;
+        this.dataInserimento = dataInserimento;
+        this.dataUltimoContatto = dataUltimoContatto;
+        this.fatturatoAnnuale = fatturatoAnnuale;
+        this.pec = pec;
+        this.telefono = telefono;
+        this.emailContatto = emailContatto;
+        this.nomeContatto = nomeContatto;
+        this.cognomeContatto = cognomeContatto;
+        this.logoAziendale = logoAziendale;
+        this.tipoAzienda = tipoAzienda;
+        this.indirizzoSedeLegale = indirizzoSedeLegale;
+        this.indirizzoSedeOperativa = indirizzoSedeOperativa;
+    }
+}
---
+++ src/main/java/Back_end/BW2/entities/Utente.java
@@ -0,0 +1,63 @@
+package Back_end.BW2.entities;
+
+import Back_end.BW2.enums.RuoloUtente;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "utenti")
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties({"password", "clientiList", "enabled", "accountNonLocked", "accountNonExpired", "credentialsNonExpired", "authorities"})
+public class Utente implements UserDetails {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+    private String username;
+    private String email;
+
+    private String password;
+    private String nome;
+    private String cognome;
+    private String avatar;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ruolo_utente")
+    private RuoloUtente ruoloUtente;
+
+    @OneToMany()
+    @JoinColumn(name = "clientiList")
+    private List<Cliente> clientiList;
+
+    public Utente(String username, String email, String password, String nome, String cognome, String avatar, RuoloUtente ruoloUtente) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.avatar = avatar;
+        this.ruoloUtente = ruoloUtente;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.ruoloUtente.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
