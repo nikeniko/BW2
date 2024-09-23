150b537b4f3ce0c2a94cdbf4115edd36ca52fe17
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-09-23 11:21:01 +0200

    Initial commit

---
+++ src/main/java/Back_end/BW2/Bw2Application.java
@@ -0,0 +1,13 @@
+package Back_end.BW2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Back_end/BW2/Bw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Back_end.BW2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
