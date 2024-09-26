491fa8fca4e7c8b3d25728d568bf4993739049c4
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-09-26 09:40:47 +0200

    add 3 filtri cliente repository

--- src/main/java/Back_end/BW2/repositories/ClientiRepository.java
+++ src/main/java/Back_end/BW2/repositories/ClientiRepository.java
@@ -4,8 +4,18 @@ import Back_end.BW2.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ClientiRepository extends JpaRepository<Cliente, UUID> {
+
+    List<Cliente> findByFatturatoAnnuale(int fatturatoAnnuale);
+
+    List<Cliente> findByDataInserimento(LocalDate dataInserimento);
+
+    List<Cliente> findByDataUltimoContatto(LocalDate dataUltimoContatto);
+
+
 }
